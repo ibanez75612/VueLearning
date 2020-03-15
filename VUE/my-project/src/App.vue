eaf18426c68351c6521f396fce1445f94feadfe8
ibanez751612 <ibanez75612@gmail.com>
2020-03-15 22:27:02 +0300

    çalışanlar listelendi ancak tekli data getirilirken sorun yaşanıyor

---
+++ VUE/my-project/src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+
+    <table class="table" v-if="showGrid==1" >
+      <tbody>
+      <tr>
+        <td>index</td>
+        <td>Çalışan</td>
+        <td>Maaş</td>
+        <td>Yaş</td>
+        <td>Resim</td>
+        <td>Detay</td>
+      </tr>
+      <tr v-for="(tb,index) in employeeListData" :key="tb.id">
+        <td>{{tb.id}}</td>
+        <td>{{tb.employee_name}}</td>
+        <td>{{tb.employee_salary}} $</td>
+        <td>{{tb.employee_age}}</td>
+        <td> <img v-bind:src="tb.profile_image" /> </td>
+        <td><button  class="btn btn-primary" v-on:click="EmployeeDetail(tb)">Detayını Gör</button></td>
+      </tr>
+      </tbody>
+    </table>
+
+    <table class="table" v-if="showSingleEmployee==1"> 
+        <tr>
+        <td>{{employeeSingleData.id}}</td>
+        <td>{{employeeSingleData.employee_name}}</td>
+        <td>{{employeeSingleData.employee_salary}}</td>
+        <td>{{employeeSingleData.employee_age}}</td>
+         <td> <img v-bind:src="employeeSingleData.profile_image" /> </td>
+        </tr>
+    </table>
+
+    <table class="table" v-else> 
+        <tr>
+        <td>Kayıt bulunamadı</td>
+        </tr>
+    </table>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: 'App',
+  created() {
+    
+  },
+  mounted() {
+    axios.get("http://dummy.restapiexample.com/api/v1/employees").then(response =>(this.employeeListData=response.data.data))
+  },
+  methods: {
+    EmployeeDetail(element){
+       this.showGrid=0;
+      //  axios.get("http://dummy.restapiexample.com/api/v1/employee/"+element.id,{ mode: 'cors', method: 'GET', dataType: 'json', headers: { 'Access-Control-Allow-Origin':'*', 'Content-type': 'application/json;charset=utf-8','Upgrade-Insecure-Requests':1}}).then(response=> (this.employeeSingleData=response.data)).catch(
+      //    function(error){
+      //      console.log(error);
+      //    }
+      //  );
+
+       fetch('http://dummy.restapiexample.com/api/v1/employee/'+element.id)
+  .then((response) => (this.employeeSingleData=response.data))
+  .then((data) => {
+    console.log(data);
+  });
+
+       this.showSingleEmployee=1
+    },
+  },
+  computed: {
+    
+  },
+  updated() {
+     console.log(this.employeeSingleData);
+  },
+  data() {
+    return {
+       employeeListData:[],
+       showGrid:1,
+       showSingleEmployee:0,
+       employeeSingleData:{}
+    }
+  },
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
